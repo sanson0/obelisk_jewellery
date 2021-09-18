eeadd2f3b4feb915d8c17a7f3f5b5c7736b4cf24
sanson0 <s04anson@gmail.com>
2021-09-18 20:18:01 +0000

    Contact us, delivery and payment method pages added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('deliver', views.deliver, name='deliver'),
+    path('payment', views.payment, name='payment'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def deliver(request):
+    return render(request, 'home/deliver.html')
+
+
+def payment(request):
+    return render(request, 'home/payment.html')
