5218ec3c0ab9f84c234412680b8483d3386ac447
sanson0 <s04anson@gmail.com>
2021-10-15 10:22:30 +0000

    Deployment part 1

--- obelisk_jewellery/settings.py
+++ obelisk_jewellery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,10 +125,11 @@ WSGI_APPLICATION = 'obelisk_jewellery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
