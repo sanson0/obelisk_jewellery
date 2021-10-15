f49958cec30f526322232348eb08434a778996de
sanson0 <s04anson@gmail.com>
2021-10-15 14:05:51 +0000

    Deployment part 2

--- obelisk_jewellery/settings.py
+++ obelisk_jewellery/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lzh1uqs%vh0@a&!%78xxf&0%uf4)(7lsrtye0#dw-+=cblin9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['obelisk-jewellery.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'obelisk_jewellery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
